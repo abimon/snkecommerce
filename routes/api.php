8764bfe0849701ca7186b70085a64b560ca74c8d
abimon <eddyabby59@gmail.com>
2026-09-10 18:41:59 +0300

    callback url

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\StoreController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(StoreController::class)->group(function () {
+    Route::get('/payments/callback/{id}', 'Callback');
+});
